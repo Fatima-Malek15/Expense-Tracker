c0c0b99dd2a6bda306a3f6c8a41c5637b1afd4ff
Fatima <fathimamalek1@gmail.com>
2025-09-27 13:54:08 +0200

    added function for adding expenses

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@ def init_db():
     conn = sqlite3.connect("expense.db") #creates file if doesnt exist
     cursor = conn.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS expenses
-    (id INTEGER PRIMARY KEY AUTO-INCREMENT,
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
     category TEXT NOT NULL,
     amount REAL NOT NULL,
     expense_date TEXT NOT NULL
@@ -12,3 +12,19 @@ def init_db():
 
     conn.commit()
     conn.close()
+if __name__ == "__main__":
+    init_db()
+    print("Database initialized")
+
+#add new expense function
+def add_expense(category, amount, expense_date):
+    conn = sqlite3.connect("expense.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO expenses"
+                   "(category, amount, expense_date "
+                   "VALUES(?, ?, ?)",
+                   (category,amount, expense_date))
+    conn.commit()
+    conn.close()
+    print("Expense added")
+
